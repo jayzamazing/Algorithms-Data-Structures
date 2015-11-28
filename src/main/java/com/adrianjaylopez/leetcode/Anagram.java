f583672b0408ae6358c0e044cf525b6a8da590fa
Jay <adrianjaylopez@gmail.com>
2015-11-27 19:58:09 -0500

    Added simple docuentation to anagram class.  Created anagramtest class to junit anagram class.

--- src/main/java/com/adrianjaylopez/leetcode/Anagram.java
+++ src/main/java/com/adrianjaylopez/leetcode/Anagram.java
@@ -1,21 +1,31 @@
 package com.adrianjaylopez.leetcode;
 
 /**
+ * This class is used to see if a word is an anagram of another word or not. O(n) time
  * Created by Jay on 11/22/15.
  */
 public class Anagram {
+    /**
+     * Method to verify if a word is an anagram of another.
+     * @param s first string to compare
+     * @param t second string to compare
+     * @return true if word is anagram false if not anagram or word lengths do not match
+     */
     public boolean isAnagram(String s, String t) {
+        //if the length of the first word does not match the second word
         if(s.length() != t.length())
             return false;
-        int [] count = new int[26];
+        int [] count = new int[26];//declare and initialize array
+        //loop while length of string is not reached
         for (int i = 0; i < s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            count[s.charAt(i) - 'a']++;//add one to array for character
+            count[t.charAt(i) - 'a']--;//substract one from array for character
         }
+        //loop over count array
         for(int c: count){
-            if (c != 0)
+            if (c != 0)//if any character in array is not zero
                 return false;
         }
-        return true;
+        return true;//true if strings are anagram
     }
 }
---
+++ src/test/java/com/adrianjaylopez/leetcode/AnagramTest.java
@@ -0,0 +1,44 @@
+package com.adrianjaylopez.leetcode;
+
+/**
+ * Class to test anagram class.
+ * Created by Jay on 11/27/15.
+ */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AnagramTest {
+    Anagram anagram;
+
+    /**
+     * This runs before each test for the initial setup
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        anagram = new Anagram();// instantiate class
+    }
+
+    /**
+     * This runs after each test to perform clean up
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        //set everything to null
+        anagram = null;
+    }
+
+    /**
+     * Test the isvoid method.
+     * @throws Exception
+     */
+    @Test
+    public void testIsVoid() throws Exception{
+        assertEquals(anagram.isAnagram("hello", "olleh"), true);//checks that true is returned for anagram
+        assertEquals(anagram.isAnagram("world", "olrdwf"), false);//check for false if both strings are not the same length
+        assertEquals(anagram.isAnagram("world", "olrdq"), false);//check for false when not an anagram
+    }
+}
